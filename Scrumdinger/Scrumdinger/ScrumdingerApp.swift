72f7c86ae6b874598511eadeb7590f9ad601c454
Jorge García Mateos <jorgegma@ext.inditex.com>
2022-11-28 19:31:41 +0100

    add project skeleton

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Jorge García Mateos on 28/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
