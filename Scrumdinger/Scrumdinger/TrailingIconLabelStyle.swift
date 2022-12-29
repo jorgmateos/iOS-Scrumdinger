1708c353fa3bae3c03348f187c67e5e7f5273669
Jorge García Mateos <jorgegmateos@gmail.com>
2022-12-29 12:53:09 +0100

    views (#4)
    * feat: create a card view
    
    * feat: display data in the list

---
+++ Scrumdinger/Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Jorge García Mateos on 13/12/22.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
